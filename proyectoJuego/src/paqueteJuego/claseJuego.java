78b6388e7b57b883f3eeceeda518615bba50a75b
franky <franky3553@gmail.com>
2020-05-26 19:21:42 +0200

    guardado pre2

---
+++ proyectoJuego/src/paqueteJuego/claseJuego.java
@@ -0,0 +1,34 @@
+package paqueteJuego;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class claseJuego extends Canvas {
+	private static final long serialVersionUID = 1L;
+	
+	private static JFrame ventana;
+	
+	private static final int ANCHO = 600;
+	
+	private static final int ALTO = 600;
+	
+	private claseJuego() {
+		setPreferredSize(new Dimension(ANCHO, ALTO));
+		
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.setResizable(false);
+		ventana.setLayout(new BorderLayout());
+		ventana.add(this, BorderLayout.CENTER);
+		ventana.pack();
+		ventana.setLocationRelativeTo(null);
+		ventana.setVisible(true);
+	}
+	
+	public static void main(String[] args) {}
+
+}
+
+
