4f20806401a5af13c3aae81d8dbf476d9dddb69f
franky <franky3553@gmail.com>
2020-06-01 20:02:45 +0200

    guardado 6 graficos hoja-sprites

---
+++ proyectoJuego/src/paqueteGraficos/claseHojaSprites.java
@@ -0,0 +1,35 @@
+package paqueteGraficos;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class claseHojaSprites {
+	private final int ancho;
+	private final int alto;
+	
+	public final int[] pixeles;
+	
+	public claseHojaSprites(final int ancho, final int alto, final String ruta) {
+		this.ancho = ancho;
+		this.alto = alto;
+		
+		pixeles = new int [ancho * alto];
+		
+		BufferedImage imagen;
+		
+		try {
+			
+			imagen = ImageIO.read(claseHojaSprites.class.getResource(ruta));
+			
+			imagen.getRGB(0, 0, ancho, alto, pixeles, 0, ancho);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
